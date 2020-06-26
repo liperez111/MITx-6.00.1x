52c861f1fa545fbb655af514a46bc97b6da77930
Luis Pérez Gómez <lp_11@live.com.mx>
2020-06-26 13:40:01 -0500

    Testing commit

--- Exercise guess my number.py
+++ Exercise guess my number.py
@@ -1,3 +1,11 @@
+#In this problem, you'll create a program that guesses a secret number!
+
+#The program works as follows: you (the user) thinks of an integer between 0 (inclusive) and 100 (not inclusive). The computer makes guesses,
+#and you give it input - is its guess too high or too low? Using bisection search, the computer will guess the user's secret number!
+#Note: your program should use input to obtain the user's input! Be sure to handle the case when the user's input is not one of h, l, or c.
+#When the user enters something invalid, you should print out a message to the user explaining you did not understand their input. Then, you should re-ask the question, and prompt again for input.
+
+
 a = 100
 b = 0
 c = int(a / 2)
