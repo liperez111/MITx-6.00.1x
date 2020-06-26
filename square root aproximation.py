c94ff98d59d2daa0fbcbc84e11ebfe904fda1f9e
Luis Pérez Gómez <lp_11@live.com.mx>
2020-06-26 16:09:54 -0500

    convert code to function

--- square root aproximation.py
+++ square root aproximation.py
@@ -1,12 +1,16 @@
 #square root aproximation
 
-epsilon = 0.0000000001
-y = 49
-guess = y/2.0
-numGuesses = 0
 
-while abs(guess*guess - y) >= epsilon:
-    numGuesses += 1
-    guess = guess - (((guess**2)-y)/(2*guess))
-print("numGuesses = " + str(numGuesses))
-print("Square root of " +  str(y) + " is about " + str(guess))
+def square(y):
+    epsilon = 0.0000000001
+    guess = y/2.0
+    numGuesses = 0
+
+    while abs(guess*guess - y) >= epsilon:
+        numGuesses += 1
+        guess = guess - (((guess**2)-y)/(2*guess))
+
+    return guess
+
+
+print(square(100))
